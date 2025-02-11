c830250aca8a3ac2d4f1a3de47ab1e0f23f224ee
joaonunez <joaovaldiglesias@gmail.com>
2025-02-11 19:21:00 -0300

    agregar canciones listo

--- src/main/java/com/example/app/controllers/SongController.java
+++ src/main/java/com/example/app/controllers/SongController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.app.models.Song;
 import com.example.app.services.SongService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class SongController {
@@ -32,5 +38,21 @@ public class SongController {
         }
         model.addAttribute("song", song);
         return "songDetail.jsp";
+    }
+    
+    @GetMapping("/songs/form/add")
+    public String formAddSong(Model model) {
+        model.addAttribute("song", new Song());
+        return "addSong.jsp";
+    }
+
+    @PostMapping("/songs/process/add")
+    public String procesarAgregarCancion(@Valid @ModelAttribute("song") Song song, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return "addSong.jsp";
+        }
+        songService.addSong(song);
+        redirectAttributes.addFlashAttribute("success", "Canción agregada con éxito.");
+        return "redirect:/songs";
     }
 }
